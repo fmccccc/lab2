10fe4489e5d37e5955e82940a83db538a4b1d588
fmc <2298981880@qq.com>
2024-11-07 19:54:54 +0800

    1

---
+++ task11/myapp/src/main/java/com/example/FundFlowStatistics.java
@@ -0,0 +1,66 @@
+package com.example;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class FundFlowStatistics {
+
+    public static class FlowMapper extends Mapper<LongWritable, Text, Text, Text> {
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String[] fields = value.toString().split(",");
+            String date = fields[1]; // 交易日期
+            String purchaseAmt = fields[4].isEmpty() ? "0" : fields[4]; // 资金流入
+            String redeemAmt = fields[9].isEmpty() ? "0" : fields[8]; // 资金流出
+            context.write(new Text(date), new Text(purchaseAmt + "," + redeemAmt));
+        }
+    }
+
+    public static class FlowReducer extends Reducer<Text, Text, Text, Text> {
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            long totalPurchase = 0;
+            long totalRedeem = 0;
+            for (Text value : values) {
+                String[] amounts = value.toString().split(",");
+                if (amounts.length > 0) {
+                    try {
+                        totalPurchase += Long.parseLong(amounts[0]);
+                        totalRedeem += Long.parseLong(amounts[1]);
+                    } catch (NumberFormatException e) {
+                        // 如果amounts[0]不能转换为Long类型，捕获异常并跳过
+                        System.out.println("Skipping non-numeric value: " + amounts[0]);
+                    }
+                }   
+
+                
+            }
+            context.write(key, new Text(totalPurchase + "," + totalRedeem));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Daily Flow Stats");
+        job.setJarByClass(FundFlowStatistics.class);
+        job.setMapperClass(FlowMapper.class);
+        job.setReducerClass(FlowReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
---
+++ task3/myapp/src/main/java/com/example/UsersActivity.java
@@ -0,0 +1,63 @@
+package com.example;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class UsersActivity {
+
+    public static class ActivityMapper extends Mapper<Object, Text, Text, IntWritable> {
+        @Override
+        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String[] fields = value.toString().split(",");
+            String userId = fields[0];
+            long directPurchase = 0;
+            long totalRedeem =0;
+            try {
+                directPurchase = Long.parseLong(fields[5]);
+                totalRedeem = Long.parseLong(fields[8]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            if (directPurchase > 0 || totalRedeem > 0) {
+                context.write(new Text(userId), new IntWritable(1));
+            }
+        }
+    }
+
+    public static class ActivityReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int activeDays = 0;
+            for (IntWritable val : values) {
+                activeDays += val.get();
+            }
+            context.write(key, new IntWritable(activeDays));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "User Activity Analysis");
+        job.setJarByClass(UsersActivity.class);
+        job.setMapperClass(ActivityMapper.class);
+        job.setReducerClass(ActivityReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
